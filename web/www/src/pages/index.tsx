6cf060305b12560cdd1a9a404ea88ee65ff1de5f
Include caleb.fun <genos@saitama.fun>
2024-12-16 00:26:32 +0100

    ...

--- web/www/src/pages/index.tsx
+++ web/www/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center space-y-4 text-center px-4">
@@ -5,9 +7,12 @@ export default function HomePage() {
         <h1 className="text-3xl font-extrabold">Human Verification</h1>
         <p className="text-base font-bold">Verify below to be granted entry</p>
       </div>
-      <button className="min-w-sm p-3 font-mono text-base rounded bg-gradient-to-r from-primary via-secondary to-tetiary hover:bg-gradient-to-br shadow-lg shadow-black/50 underline dark:shadow-lg dark:shadow-black/80">
+      <Link
+        to="/auth"
+        className="min-w-sm p-3 font-mono text-base rounded bg-gradient-to-r from-primary via-secondary to-tetiary hover:bg-gradient-to-br shadow-lg shadow-black/50 underline dark:shadow-lg dark:shadow-black/80"
+      >
         Click here
-      </button>
+      </Link>
     </main>
   );
 }
