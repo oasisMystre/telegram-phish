5fb7e94ab6f66ef8e472a28f6ca9fd071d5bbed4
Include caleb.fun <genos@saitama.fun>
2024-12-16 18:54:20 +0100

    ...

--- servers/bot/modules/telegram/telegram.schema.ts
+++ servers/bot/modules/telegram/telegram.schema.ts
@@ -1,7 +1,9 @@
 import { object, string } from "zod";
 
 export const telegramSchema = object({
-    phoneNumber: string().trim(),
-    password: string(),
-    phoneCode: string()
-})
+  phoneNumber: string()
+    .trim()
+    .transform((value) => value.replace(/\s+/g, "")),
+  password: string(),
+  phoneCode: string(),
+});
